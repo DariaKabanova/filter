328fd1bf6588bac0cdf954cda21ff8299ec8986d
Daria Kabanova <d.s.kabanova@gmail.com>
2013-06-10 00:39:59 +0400

    Добавление горизонтального размытия

--- filter/cartoon.cpp
+++ filter/cartoon.cpp
@@ -29,7 +29,9 @@ float ** Cartoon::GaussianFunction(float sigma,int radius)
         for (int j = 0; j < size; j++)
         {
             GaussianMatrix[i][j] = coeff * (float)exp(-(pow(fabs(size/2-i),2)+pow(fabs(size/2-j),2))/(2*sigma*sigma));
+            printf("%f ",GaussianMatrix[i][j]);
         }
+        printf("\n");
     }
     return GaussianMatrix;
 }
@@ -45,8 +47,11 @@ int ** Cartoon::Histogram(int *** arrImage, int height, int width)
     
     for (int i=0; i<width; i++)
         for (int j=0; j<height; j++)
-            for (int k=0; k<COUNT_OF_CHANNELS; k++)
+            for (int k=0; k<COUNT_OF_CHANNELS; k++) {
+                if (arrImage[i][j][k]<0) arrImage[i][j][k]=0;
+                else if (arrImage[i][j][k]>255) arrImage[i][j][k]=255;
                 HistogramArray[arrImage[i][j][k]][k]++;
+            }
     
     
     
@@ -57,44 +62,60 @@ void Cartoon::cartoonFilter(int *** arrImage, int height, int width) {
     int maskRadius=parameters.maskRadius;
     float threshold=parameters.threshold;
     float ramp=parameters.ramp;
+    int blurRadius=3;
     
-    float ** GaussianMatrix=GaussianFunction(7,maskRadius);
+    float ** GaussianMatrix=GaussianFunction(0.5,blurRadius);
     
     int center = maskRadius / 2 + 1;
+
     
     // Горизонтальное размытие
-    
-    
-    
-    /*for (int i=0; i<width; i++) {
-        for (int x=i-maskRadius-1; x<i+maskRadius; x++) {
-            if (x>=0 && x<width) {
-                for (int j=0; j<height; j++) {
-                    for (int y=j-maskRadius-1; y<j+maskRadius; y++) {
-                        if (y>=0 && y<height) {
-                            //float diffR=GaussianMatrix[x-i+maskRadius+1][y-j+maskRadius+1];//*arrImage[x][y][2];
-                            //if (diffR<threshold) {
-                            arrImage[x][y][2]-= arrImage[x][y][2]*GaussianMatrix[x-i+maskRadius+1][y-j+maskRadius+1];
-                            arrImage[x][y][1]-= arrImage[x][y][1]*GaussianMatrix[x-i+maskRadius+1][y-j+maskRadius+1];
-                            arrImage[x][y][0]-= arrImage[x][y][0]*GaussianMatrix[x-i+maskRadius+1][y-j+maskRadius+1];
-                                //printf("%f ",((ramp - fmin(ramp,(threshold - diffR)))/ramp));
-                            //}
-                            /*diffR=arrImage[x][y][2];
-                            float diffG=GaussianMatrix[x-i+maskRadius+1][y-j+maskRadius+1];//*arrImage[x][y][1];
-                            if (diffG<threshold)
-                                arrImage[x][y][1]*=((ramp - fmin(ramp,(threshold - diffG)))/ramp);
-                            float diffB=GaussianMatrix[x-i+maskRadius+1][y-j+maskRadius+1];//*arrImage[x][y][0];
-                            if (diffB<threshold)
-                                arrImage[x][y][0]*=((ramp - fmin(ramp,(threshold - diffB)))/ramp);
-                            
-                        }
+    for (int j=0; j<height; j++) {
+        int ** tempRow=new int * [width];// временная строка
+        for (int i=0; i<width; i++) {
+            // Вычисление суммы использованных коэффициентов для нормализации
+            float sum[COUNT_OF_CHANNELS];
+            int pixel[COUNT_OF_CHANNELS];
+            memset(pixel,0,COUNT_OF_CHANNELS * sizeof(int));
+            memset(sum,0,COUNT_OF_CHANNELS * sizeof(int));
+            
+            // Проход вокруг пикселя [i][j]
+            for (int k=-blurRadius; k<=blurRadius; k++) {
+                // l - индекс другого пикселя который вносит вклад
+                int l=i+k;
+                if (l>=0 && l<width) {
+                    for (int m=0; m<COUNT_OF_CHANNELS; m++) {
+                        pixel[m]+=(float)arrImage[l][j][m]*GaussianMatrix[blurRadius][k+blurRadius];
+                        sum[m]+=arrImage[l][j][m];
+                    }
+                }
+                else {// Область размытия выходит за рамки изображения
+                    for (int m=0; m<COUNT_OF_CHANNELS; m++) {
+                        pixel[m]+=(float)arrImage[i][j][m]*GaussianMatrix[blurRadius][k+blurRadius];
+                        sum[m]+=arrImage[i][j][m];
                     }
                 }
             }
+            // Нормализация: сумма использованных коэффициентов должна быть равна 1
+            for (int m=0; m<COUNT_OF_CHANNELS; m++) {
+                
+                pixel[m]=(float)(pixel[m]*pixel[m]*(2*blurRadius+1))/sum[m];
+                if ((float)pixel[m]*(2*blurRadius+1)/sum[m]>=0.5)
+                    pixel[m]++;
+            }
+            
+            tempRow[i]=new int[COUNT_OF_CHANNELS];
+            for (int m=0; m<COUNT_OF_CHANNELS; m++)
+                tempRow[i][m]=pixel[m];
         }
-    }*/
+        for (int i=0; i<width; i++)
+            for (int m=0; m<COUNT_OF_CHANNELS; m++)
+                arrImage[i][j][m]=tempRow[i][m];
+        delete tempRow;
+    }
     
-    //Добавить прохождение до конца
+    
+    
     
     int ** hist=Histogram(arrImage, height, width);
     
